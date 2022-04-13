bfea8e5edfe5a8b41c24805fdab06301482b3289
Charly M <46685191+CharlyMoo@users.noreply.github.com>
2022-04-12 23:21:21 -0500

    Create Modulus_operand_error.cpp
    HOW to not use mod (%)

---
+++ Modulus_operand_error.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float operand1 = 8;
+  float operand2 = 2;
+  cout << "Modulus = " << operand1 % operand2 << endl;
+  return 0;
+} 
+
+// Using mod with 2 operands float will give an error
